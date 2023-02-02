9ef328cc484c0c61ef9b2dd311c90a329ae43719
gustavogmail <gustavograal@gmail.com>
2023-02-01 23:51:58 -0300

    Trying4

--- client/src/service/Api.ts
+++ client/src/service/Api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 export default ()=>{
-    const PORT = process.env.PORT || 3001;
     return axios.create({
-        baseURL:`http://localhost:${PORT}`
+        baseURL:'http://localhost:10000'
     })
 }
