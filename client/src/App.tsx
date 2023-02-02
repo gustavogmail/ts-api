282a40cc6229e82858b6156282efca863cf95915
gustavogmail <gustavograal@gmail.com>
2023-02-01 21:14:36 -0300

    node build

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './components/Home';
 import UserDetails from './components/UserDetails';
+import Dashboard from './components/dashboard/Dashboard';
 
 import {
   createBrowserRouter,
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
     path: "/:contactId",
     element: <UserDetails />,
   },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  }
 ]);
 
 function App() {
---
+++ client/src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+const Dashboard = () => {
+    return (
+        <>
+            <div className="container">
+                <img src="/poliptico8.png" className="img-fluid" alt="ALTER"/>
+            </div>
+        </>
+    );
+};
+
+export default Dashboard;
